013c23e545d22714250462d9ca462c192fac7a5b
Nicholas Gulley <npgulley@ksu.edu>
2022-01-18 13:58:10 -0600

    Completed ball demo

---
+++ Hello Game/Hello Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_Game
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new HelloGame())
+                game.Run();
+        }
+    }
+}
